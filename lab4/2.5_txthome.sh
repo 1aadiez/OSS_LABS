7cb656cbd6f130c0b957b72597daea14978cc820
1aadiez <84510233+1aadiez@users.noreply.github.com>
2022-12-20 21:32:58 +0300

    mistakes

--- lab4/2.5_txthome.sh
+++ lab4/2.5_txthome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fin ~ -type f -name ".txt" > /tmp/some.txt 2>/dev/null
+find ~ -type f -name ".txt" > /tmp/some.txt 2>/dev/null
 cat /tmp/some.txt | wc -l
 du -h /tmp/some.txt | cut -f1
 rm /tmp/some.txt
