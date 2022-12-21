cd716755dd18b51d23ab444aee23f9653dd58691
1aadiez <84510233+1aadiez@users.noreply.github.com>
2022-12-21 22:52:54 +0300

    1.11 - updated mistakes

--- lab4/1.11.sh
+++ lab4/1.11.sh
@@ -5,7 +5,7 @@ ls -l | grep ^d
 echo "Обычные файлы:"
 ls -l | grep ^-
 echo "Символьные ссылки:"
-ls -l | grep ^|
+ls -l | grep ^l
 echo "Символьные устройства:"
 ls -l | grep ^c
 echo "Блочные устройства:"
